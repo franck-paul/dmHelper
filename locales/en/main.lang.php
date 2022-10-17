f8415ba1b0308ae5b586edb3024eaad6c140f0fc
franck <carnet.franck.paul@gmail.com>
2022-10-17 11:01:41 +0200

    Add compiled locales

---
+++ locales/en/main.lang.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package Dotclear
+ *
+ * @copyright Olivier Meunier & Association Dotclear
+ * @copyright GPL-2.0-only
+ */
+#
+# DOT NOT MODIFY THIS FILE !
+#
+
+l10n::$locales['Dashboard Module Helper Library'] = '';
+l10n::$locales['Helper library for Dashboard Modules'] = '';
---
+++ locales/fr/main.lang.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package Dotclear
+ *
+ * @copyright Olivier Meunier & Association Dotclear
+ * @copyright GPL-2.0-only
+ */
+#
+# DOT NOT MODIFY THIS FILE !
+#
+
+l10n::$locales['Dashboard Module Helper Library'] = 'Module d\'affichage : Biblothèque utilitaire';
+l10n::$locales['Helper library for Dashboard Modules'] = 'Biblothèque utilitaire pour les modules de tableau de bord';
