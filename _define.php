81ea9874e53b22a73d319a72435ce907207bb723
franck <carnet.franck.paul@gmail.com>
2024-06-17 12:30:18 +0200

    New release 4.2

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'Dashboard Module Helper Library',
     'Helper library for Dashboard Modules',
     'Franck Paul',
-    '4.1',
+    '4.2',
     [
         'requires'    => [['core', '2.28']],
         'permissions' => 'My',
