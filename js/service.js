5ff94d1b77b8334e57ffbeae45f07cc597473900
franck <carnet.franck.paul@gmail.com>
2024-06-17 12:28:16 +0200

    Add dotclear.dmOnline() helper (check if connection is on/off, unknown)

--- js/service.js
+++ js/service.js
@@ -1,2 +1,9 @@
 /*global $, dotclear */
 'use strict';
+
+/**
+ * Check connection
+ *
+ * @return     {?boolean}  true/false depending on connection, null if cannot be tested
+ */
+dotclear.dmOnline = () => 'onLine' in navigator ? navigator.onLine : null;
