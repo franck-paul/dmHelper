71bd5ba2a1f8db637285c0f2344afdd40375690a
franck <carnet.franck.paul@gmail.com>
2024-12-28 06:44:02 +0100

    PHP code review (Rector/PHPStan)

--- src/Backend.php
+++ src/Backend.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @brief dmHelper, a plugin for Dotclear 2
  *
@@ -22,7 +23,8 @@ class Backend extends Process
     public static function init(): bool
     {
         // dead but useful code, in order to have translations
-        __('Dashboard Module Helper Library') . __('Helper library for Dashboard Modules');
+        __('Dashboard Module Helper Library');
+        __('Helper library for Dashboard Modules');
 
         return self::status(My::checkContext(My::BACKEND));
     }
